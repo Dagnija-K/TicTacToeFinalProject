c82aee15f4127e67a11d738c812968add38a7d61
Dagnija-K <dagnijakaleja@gmail.com>
2021-04-22 14:56:13 +0300

    Final version for two player TicTacToe

--- src/main/scala/com/github/Dagnija/TicTacToe.scala
+++ src/main/scala/com/github/Dagnija/TicTacToe.scala
@@ -1,15 +1,19 @@
+package com.github.Dagnija
+
 import scala.collection.mutable
 import scala.io.StdIn.readLine
 
 object TicTacToe {
+
   class GameState(var playground: mutable.SortedSet[Int] = collection.mutable.SortedSet(1, 2, 3, 4, 5, 6, 7, 8, 9), //total playground size 9
-                  val minMove:Int = 1,
-                  val maxMove:Int = 9,
+                  val minMove: Int = 1,
+                  val maxMove: Int = 9,
                   var isPlayerXTurn: Boolean = true, //two players X and O, X starts the game
                   var playerXMoves: mutable.Set[Int] = collection.mutable.Set[Int](),
                   var playerOMoves: mutable.Set[Int] = collection.mutable.Set[Int](),
                   val winningSets: Seq[Set[Int]] = Seq(Set(1, 2, 3), Set(4, 5, 6), Set(7, 8, 9), Set(1, 4, 7), Set(2, 5, 8), Set(3, 6, 9), Set(1, 5, 9), Set(3, 5, 7))
                  )
+
   val state = new GameState()
   //  def init():GameState = {
 
@@ -19,9 +23,9 @@ object TicTacToe {
   print(
     0 to 2 map { r =>
       0 to 2 map { c =>
-        startBoard(c + r*3)
+        startBoard(c + r * 3)
       } mkString " | "
-    } mkString ("_________\n", "\n---------\n", "\n"))
+    } mkString("_________\n", "\n---------\n", "\n"))
 
   val playerX = readLine(s"What is your name Player X?").trim.toString
   val playerO = readLine(s"What is your name Player O?").trim.toString
@@ -29,8 +33,8 @@ object TicTacToe {
   def getPlayerTurn(isPlayerXTurn: Boolean): String = if (isPlayerXTurn) playerX else playerO
 
   def containsSubset(playerMoves: mutable.Set[Int]): Boolean = {
-    for( winningSet <- state.winningSets) {
-      if(winningSet.subsetOf(playerMoves)) {
+    for (winningSet <- state.winningSets) {
+      if (winningSet.subsetOf(playerMoves)) {
         return true
       }
     }
@@ -57,9 +61,11 @@ object TicTacToe {
     state.playground -= movesMade
   }
   if (containsSubset(state.playerXMoves)) {
-    println("Player X, you win")}
+    println("Player X, you win")
+  }
   else if (containsSubset(state.playerOMoves)) {
-    println("Player O, you win")}
+    println("Player O, you win")
+  }
   else println("It is a draw. There is no winner.")
 
 }
